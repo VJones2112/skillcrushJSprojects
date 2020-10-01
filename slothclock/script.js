54fe54fb8663d8f56f5785b6afbdb73b3ac42584
Veronica Jones <veronicajones2112@gmail.com>
2020-10-01 16:21:05 -0500

    Cleaned up some duplicate code. Still working on getting the values from the drop down menu to trigger the image changes.

--- slothclock/script.js
+++ slothclock/script.js
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
 // Declare Variables
 // Clock Variables
 let currentTime, currentHours, currentMinutes, currentSeconds, timeOfDay, currentTimeString; 
 // Time Change Variables
-let wakeyWakeTime, nappyNapTime, snackySnackTime, displayTime, message, image
-
+let wakeyWakeTime, nappyNapTime, snackySnackTime, displayTime, message, image;
+currentTime = new Date();
+currentHours = currentTime.getHours();
 
 // Creates the clock
 function updateClock() {
-    currentTime = new Date();
-    currentHours = currentTime.getHours();
+
+    
     currentMinutes = currentTime.getMinutes();
     currentSeconds = currentTime.getSeconds();
     
@@ -28,15 +28,15 @@ function updateClock() {
     currentTimeString = currentHours + ':' + currentMinutes + ':' + currentSeconds + ' ' + timeOfDay;
     
     // Display the clock in html
-    document.getElementById("runningClock").innerHTML = currentTimeString
+    document.getElementById("runningClock").innerHTML = currentTimeString;
 };
 
 
 
 //Updates clock so it runs seconds continuously 
 window.onload = updateClock();
-setInterval('updateClock()', 1000)
- 
+setInterval('updateClock()', 1000);
+
 
 
 // Changes image depending if it's morning, afternoon, or evening
@@ -89,71 +89,30 @@ function imageChange() {
 imageChange();
 
 
-// New party image when Party Time! button is pressed
-function partyTimeImage() {
-    let element = document.getElementById("changeImage");
-    element.classList.toggle("partyImage");
-=======
-// Creates the clock
-function updateClock() {
-    let currentTime = new Date();
-    let currentHours = currentTime.getHours();
-    let currentMinutes = currentTime.getMinutes();
-    let currentSeconds = currentTime.getSeconds();
-    // Format the time
-    currentMinutes = (currentMinutes < 10 ? '0' : '') + currentMinutes;
-    currentSeconds = (currentSeconds < 10 ? '0' : '') + currentSeconds;
-    // Set up AM PM
-    let timeOfDay = (currentHours < 12) ? 'AM' : 'PM';
-    // Use 12 hour clock vs. 24 hour
-    currentHours = (currentHours > 12) ? currentHours - 12 : currentHours;
-    currentHours = (currentHours == 0) ? 12 : currentHours;
-    // Join time into one string as HH:MM:SS AM/PM
-    let currentTimeString = currentHours + ':' + currentMinutes + ':' + currentSeconds + ' ' + timeOfDay;
-    // Display the clock in html
-    document.getElementById("runningClock").innerHTML = currentTimeString
-}
-//Updates clock so it runs seconds continuously 
-window.onload = updateClock();
-setInterval('updateClock()', 1000)
+// Get the selected times for the Drop Down menus to work
+// Activates Wake-Up selector
+var wakeUpTimeSelector =  document.getElementById("wakeTimes");
 
-// Changes image depending if it's morning, afternoon, or evening
-//let wakeyWakeTime = 4;
-let wakeyWakeTime = document.querySelector('.wake')
-let nappyNapTime = 2;
-let snackySnackTime = wakeyWakeTime + 2;
-let time = new Date().getHours(); //I only care about the hours
-//let message;
+var wakeUpEvent = function()
+{
+    wakeyWakeTime = wakeUpTimeSelector.value;
+    console.log(wakeUpTimeSelector.value)
+    imageChange();
+};
 
-function imageChange() {
-//    if time == wakeyWakeTime {
-//        //let wake = document.getElementById("changeImage");
-//        //wake.classList.toggle("partyImage");
-//        message = document.getElementById("messageText");
-//        message = 'Rise and shine!'
+wakeUpTimeSelector.addEventListener("change", wakeUpEvent);
+
+//var selectedWakeUpTime = document.getElementById('wakeTimes').addEventListener('change', function() {
+//    wakeyWakeTime = selectedWakeUpTime.value;
+//});
+//let selectedWakeUpTime = document.getElementById('wakeTimes').addEventListener('change', changeWake);
+//function changeWake() {
+//    console.log(selectedWakeUpTime.options[selectedWakeUpTime.selectedIndex].value)
+//    wakeyWakeTime = selectedWakeUpTime.value; // Cannot read property 'value' of undefined
 //}
-    
-
- if time == wakeyWakeTime {
- document.getElementById('changeImage').src='images/wakesloth.png'
- message = 'Rise and shine!'
-}
-
- if time == snacktime {
- image = snack image
- message = 'Time to get your nomnom on!'
-}
-
- if time == naptime {
- image = napimage
- message = 'Let's snuggle'
-}
-
-}
 
 // New party image when Party Time! button is pressed
 function partyTimeImage() {
     let element = document.getElementById("changeImage");
     element.classList.toggle("partyImage");
->>>>>>> 7157db445e64e419367e72bb113340567be45559
 }
